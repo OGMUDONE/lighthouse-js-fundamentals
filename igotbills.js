c1ebf3137ef19a5b7beb0a8b0300e7465801e191
Yvon Vanya Rumyansev <vanyarumyansev@gmail.com>
2022-11-03 22:58:43 +0000

    Udacity Arrays

---
+++ igotbills.js
@@ -0,0 +1,13 @@
+var bills = [50.23, 19.12, 34.01,
+  100.11, 12.15, 9.90, 29.11, 12.99,
+  10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+
+let totals = bills.map(function (item) {
+  item *= 1.15;
+  item = item.toFixed(2);
+  item = Number(item)
+  return item;
+});
+
+console.log(totals);
---
+++ rainbow.js
@@ -0,0 +1,5 @@
+var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
+
+rainbow.splice(2, 1, "Yellow", "Green");
+rainbow.splice(-1, 1, 'Blue', 'Purple');
+console.log(rainbow);
