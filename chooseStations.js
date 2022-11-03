9a93ab087528d31c35311a7f5354632cfdfe0799
Yvon Vanya Rumyansev <vanyarumyansev@gmail.com>
2022-11-03 00:07:03 +0000

    Voting Station Calculation

---
+++ chooseStations.js
@@ -0,0 +1,30 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'], 
+  ['Moose Mountain Community Centre', 45, 'community centre'],
+  ['Codeville Epic Academy', 15, 'school'],
+  ['Big Al Pizza', 50, 'restaurant']
+]
+
+// Stations must have a capacity of at least 20
+// Stations must be a school or community centre
+
+function chooseStations (stations) {
+  const goodStations = []
+  for (const station of stations) {
+    const capacity = station[1]
+
+    if (capacity >= 20) {
+      
+      const type = station[2]
+      
+      if (type === 'school' || type == 'community centre') {
+        goodStations.push(station[0])
+      }
+    }
+  }
+
+  return goodStations;
+}
+
+console.log(chooseStations(stations));
